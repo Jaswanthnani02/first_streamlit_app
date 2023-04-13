b68a39f78964f952c87b61101f471527c8a70f86
Jaswanthnani02 <98007770+Jaswanthnani02@users.noreply.github.com>
2023-04-13 17:37:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 streamlit.title('My parents New heathy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Dosa & Iddiy')
-
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍳Hard-Boiled Free-Range Egg')
+streamlit.text('🥑Av0cado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
